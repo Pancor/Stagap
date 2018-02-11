3c7c841f8e3258a6fc545d066c3eae2ce4e4d730
Oktawian Kausz <play26400@gmail.com>
2018-02-11 19:19:27 +0100

    Added changing fragments and firebase

--- app/src/main/java/com/stagap/stagap/MainActivity.java
+++ app/src/main/java/com/stagap/stagap/MainActivity.java
@@ -1,44 +1,63 @@
 package com.stagap.stagap;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private HomeFragment homeFragment;
+    private NotificationsFragment notificationsFragment;
+    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        homeFragment = new HomeFragment();
+        notificationsFragment = new NotificationsFragment();
+        accountFragment = new AccountFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav = findViewById(R.id.navigation);
+        mMainNav.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case(R.id.navigation_home):
+                        setFragment(homeFragment);
+                        return true;
+                    case(R.id.navigation_person):
+                        setFragment(accountFragment);
+                        return true;
+                    case(R.id.navigation_notifications):
+                        setFragment(notificationsFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+
+                }
+        });
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
     }
 
 }
