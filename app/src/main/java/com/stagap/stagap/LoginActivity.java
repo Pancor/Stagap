0bc33accbdd3e1365cc75a13f02241a0854336d9
Oktawian Kausz <play26400@gmail.com>
2018-02-13 06:16:40 +0100

    Change LoginActivity style v2

--- app/src/main/java/com/stagap/stagap/LoginActivity.java
+++ app/src/main/java/com/stagap/stagap/LoginActivity.java
@@ -6,13 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInBtn = findViewById(R.id.signInBtn);
 
 
 
