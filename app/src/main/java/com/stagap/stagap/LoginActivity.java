51c6a256b73a130a7918cba7a6750d734d57b4e3
Oktawian Kausz <play26400@gmail.com>
2018-02-15 20:46:08 +0100

    Perfecting login process

--- app/src/main/java/com/stagap/stagap/LoginActivity.java
+++ app/src/main/java/com/stagap/stagap/LoginActivity.java
@@ -1,13 +1,9 @@
 package com.stagap.stagap;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -20,12 +16,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
     private Button signInBtn;
     private Button signUpBtn;
     private EditText emailField;
     private EditText passwordField;
     private RelativeLayout relativeLoading;
+
+    private String email;
+    private String password;
 
     private FirebaseAuth auth;
 
@@ -40,30 +42,26 @@ public class LoginActivity extends AppCompatActivity {
         signUpBtn = findViewById(R.id.signUpButton);
         relativeLoading= findViewById(R.id.relativeLoading);
 
+
         signInBtn = findViewById(R.id.signInBtn);
         signInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = emailField.getText().toString();
-                String password = passwordField.getText().toString();
+                email = emailField.getText().toString();
+                password = passwordField.getText().toString();
                 if(!TextUtils.isEmpty(password) && !TextUtils.isEmpty(email)){
-                    final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                            R.style.Theme_AppCompat_Light_Dialog);
-                    relativeLoading.setVisibility(View.VISIBLE);
-                    setEditable(false);
+                    setLoading(true);
                     auth.signInWithEmailAndPassword(email, password).addOnCompleteListener
                             (new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
                                         Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                                        relativeLoading.setVisibility(View.GONE);
-                                        setEditable(true);
+                                        setLoading(false);
                                         startActivity(intent);
                                         finish();
-                                    }else{
-                                        relativeLoading.setVisibility(View.GONE);
-                                        setEditable(true);
+                                    } else {
+                                        setLoading(false);
                                         Toast.makeText(LoginActivity.this, "Login error",
                                                 Toast.LENGTH_SHORT).show();
                                     }
@@ -88,11 +86,16 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void setEditable(Boolean truefalse){
-        signUpBtn.setClickable(truefalse);
-        signInBtn.setClickable(truefalse);
-        emailField.setEnabled(truefalse);
-        passwordField.setEnabled(truefalse);
+    private void setLoading(Boolean truefalse){
+        if(truefalse.equals(true)){
+            relativeLoading.setVisibility(View.VISIBLE);
+        }else{
+            relativeLoading.setVisibility(View.GONE);
+        }
+        signUpBtn.setClickable(!truefalse);
+        signInBtn.setClickable(!truefalse);
+        emailField.setEnabled(!truefalse);
+        passwordField.setEnabled(!truefalse);
     }
 
 }
