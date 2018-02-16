47feb65dc16dc213973ab9037150898985e37d97
Oktawian Kausz <play26400@gmail.com>
2018-02-16 20:59:30 +0100

    Login process

--- app/src/main/java/com/stagap/stagap/LoginActivity.java
+++ app/src/main/java/com/stagap/stagap/LoginActivity.java
@@ -1,101 +1,41 @@
 package com.stagap.stagap;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.PhoneNumberUtils;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button signInBtn;
-    private Button signUpBtn;
-    private EditText emailField;
-    private EditText passwordField;
-    private RelativeLayout relativeLoading;
-
-    private String email;
-    private String password;
-
-    private FirebaseAuth auth;
+    private FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        auth = FirebaseAuth.getInstance();
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        setFragment(new LoginFragment());
+    }
 
-        emailField = findViewById(R.id.emailField);
-        passwordField = findViewById(R.id.passwordField);
-        signUpBtn = findViewById(R.id.signUpButton);
-        relativeLoading= findViewById(R.id.relativeLoading);
-
-
-        signInBtn = findViewById(R.id.signInBtn);
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                email = emailField.getText().toString();
-                password = passwordField.getText().toString();
-                if(!TextUtils.isEmpty(password) && !TextUtils.isEmpty(email)){
-                    setLoading(true);
-                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener
-                            (new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
-                                        setLoading(false);
-                                        startActivity(intent);
-                                        finish();
-                                    } else {
-                                        setLoading(false);
-                                        Toast.makeText(LoginActivity.this, "Login error",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                }else{
-                    Toast.makeText(LoginActivity.this, "The fields are empty",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
+    public void onClick(View view){
+        int id = view.getId();
+        switch(id){
+            case(R.id.baseLoginBtn):
+                LoginFragment loginFragment = new LoginFragment();
+                setFragment(loginFragment);
+                break;
+            case(R.id.baseRegBtn):
+                RegisterFragment registerFragment = new RegisterFragment();
+                setFragment(registerFragment);
+                break;
+        }
 
     }
 
-    private void setLoading(Boolean truefalse){
-        if(truefalse.equals(true)){
-            relativeLoading.setVisibility(View.VISIBLE);
-        }else{
-            relativeLoading.setVisibility(View.GONE);
-        }
-        signUpBtn.setClickable(!truefalse);
-        signInBtn.setClickable(!truefalse);
-        emailField.setEnabled(!truefalse);
-        passwordField.setEnabled(!truefalse);
+    private void setFragment(Fragment fragment){
+        fragmentTransaction.replace(R.id.loginRegFragment, fragment);
+        fragmentTransaction.commit();
+
     }
 
 }
---
+++ app/src/main/java/com/stagap/stagap/LoginFragment.java
@@ -0,0 +1,91 @@
+package com.stagap.stagap;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class LoginFragment extends Fragment {
+    private Button signInBtn;
+    private EditText emailField;
+    private EditText passwordField;
+    private RelativeLayout relativeLoading;
+
+    private String email;
+    private String password;
+
+    private FirebaseAuth auth;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        auth = FirebaseAuth.getInstance();
+
+        emailField = view.findViewById(R.id.emailField);
+        passwordField = view.findViewById(R.id.passwordField);
+        relativeLoading= view.findViewById(R.id.relativeLoading);
+
+
+        signInBtn = view.findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = emailField.getText().toString();
+                password = passwordField.getText().toString();
+                if(!TextUtils.isEmpty(password) && !TextUtils.isEmpty(email)){
+                    setLoading(true);
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener
+                            (new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                                        setLoading(false);
+                                        startActivity(intent);
+                                        getActivity().finish();
+                                    } else {
+                                        setLoading(false);
+                                        Toast.makeText(getActivity(), "Login error",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }else{
+                    Toast.makeText(getActivity(), "The fields are empty",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void setLoading(Boolean truefalse){
+        if(truefalse.equals(true)){
+            relativeLoading.setVisibility(View.VISIBLE);
+        }else{
+            relativeLoading.setVisibility(View.GONE);
+        }
+        signInBtn.setClickable(!truefalse);
+        emailField.setEnabled(!truefalse);
+        passwordField.setEnabled(!truefalse);
+    }
+
+}
